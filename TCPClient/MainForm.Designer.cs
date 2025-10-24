929ef1e87195c68c17b7e9fe0377b4bb47e2f670
24521320-ThuanPhat <24521320@gm.uit.edu.vn>
2025-10-24 11:52:05 +0700

    xong mainform

--- TCPClient/MainForm.Designer.cs
+++ TCPClient/MainForm.Designer.cs
@@ -127,7 +127,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(643, 450);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
--- TCPClient/MainForm.cs
+++ TCPClient/MainForm.cs
@@ -1,20 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TCPClient
 {
-    public partial class MainForm: Form
+    public partial class MainForm : Form
     {
+        private Form _loginForm;
+        private string _userName;
+        private string _userEmail;
+        private string _userBirthday;
+
         public MainForm()
         {
             InitializeComponent();
+
+            _userName = "";
+            _userEmail = "";
+            _userBirthday = "";
+            _loginForm = null; 
+
+            this.Load += new System.EventHandler(this.MainForm_Load);
+
+
+            this.button1.Click += (sender, e) => { Application.Exit(); };
+
+
+            this.FormClosed += (sender, e) => { Application.Exit(); };
+        }
+        public MainForm(Form loginForm, string userName, string email, string birthday)
+        {
+            InitializeComponent();
+
+            _loginForm = loginForm;
+            _userName = userName;
+            _userEmail = email;
+            _userBirthday = birthday;
+
+            this.Load += new System.EventHandler(this.MainForm_Load);
+
+            this.button1.Click += new System.EventHandler(this.button1_Click_Real);
+
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed_Real);
+        }
+
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = _userName;
+            textBox2.Text = _userEmail;
+            textBox3.Text = _userBirthday;
+
+            textBox1.ReadOnly = true;
+            textBox2.ReadOnly = true;
+            textBox3.ReadOnly = true;
+        }
+
+        private void button1_Click_Real(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MainForm_FormClosed_Real(object sender, FormClosedEventArgs e)
+        {
+            if (_loginForm != null)
+            {
+                _loginForm.Show();
+            }
         }
     }
 }
